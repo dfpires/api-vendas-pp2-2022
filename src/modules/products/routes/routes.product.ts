76a79c1ad61de4b59250c9170b655a40c9dd1ec6
Daniel Pires <dfpires@gmail.com>
2022-08-31 20:12:20 -0300

    Add files via upload

--- src/modules/products/routes/routes.product.ts
+++ src/modules/products/routes/routes.product.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import router from "../../../routes";
 import ProductController from "../controllers/ProductController";
 
 
@@ -9,5 +10,7 @@ const controllerProduct = new ProductController()
 
 // está criada a rota para inserir um produto no banco de dados
 routerProduct.post('/', controllerProduct.create)
+
+routerProduct.get('/', controllerProduct.list)
 
 export default routerProduct
